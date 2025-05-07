ca4d0a6c076eccefaa4d25b78c449a6c5f4f800e
Mauricio Noriega <mauricio.noriega@iteso.mx>
2025-05-06 18:57:19 -0600

    Return after validating token in middleware

--- backend/src/middleware/auth.js
+++ backend/src/middleware/auth.js
@@ -3,7 +3,10 @@ require("dotenv").config();
 
 module.exports = (req, res, next) => {
   const token = req.get("x-auth");
-  if (!token) res.status(401).json({ message: "Token no proporcionado" });
+  if (!token) {
+    res.status(401).json({ message: "Token no proporcionado" });
+    return;
+  }
 
   try {
     const decoded = jwt.verify(token, process.env.TOKEN_SECRET);
