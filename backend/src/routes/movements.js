b54f0ae8772fa541d2e98566e3590d8c1680481d
Mauricio Noriega <mauricio.noriega@iteso.mx>
2025-05-06 18:55:39 -0600

    Use auth in movements routes

--- backend/src/routes/movements.js
+++ backend/src/routes/movements.js
@@ -3,15 +3,10 @@ const router = express.Router();
 const auth = require("../middleware/auth");
 const controller = require("../controllers/movementController");
 
-/* router.post('/', auth, controller.createMovement);
-router.get('/:id', auth, controller.getMovement);
-router.put('/:id', auth, controller.updateMovement);
-router.delete('/:id', auth, controller.deleteMovement); */
-
-router.post("/", controller.createMovement);
-router.get("/", controller.getMovements);
-router.get("/:id", controller.getMovement);
-router.put("/:id", controller.updateMovement);
-router.delete("/:id", controller.deleteMovement);
+router.post("/", auth, controller.createMovement);
+router.get("/", auth, controller.getMovements);
+router.get("/:id", auth, controller.getMovement);
+router.put("/:id", auth, controller.updateMovement);
+router.delete("/:id", auth, controller.deleteMovement);
 
 module.exports = router;
