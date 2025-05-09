229fb12ef929231e83de186c4aa4341dbdb0ae29
Mauricio Noriega <mauricio.noriega@iteso.mx>
2025-05-09 09:39:44 -0600

    Update cors

--- backend/index.js
+++ backend/index.js
@@ -2,12 +2,20 @@ require("dotenv").config();
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const morgan = require("morgan");
+const cors = require("cors");
 const movementRoutes = require("./src/routes/movements");
 const usersRoutes = require("./src/routes/users");
 const authRoutes = require("./src/routes/auth");
 
 const port = 80;
 const app = express();
+app.use(
+  cors({
+    origin: "http://3.15.53.35", // IP pública de tu frontend
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
